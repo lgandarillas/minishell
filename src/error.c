bfa4b573a05235fc366fe24e8254972808d2f8d3
lgandarillas <lgandaf@gmail.com>
2024-07-11 19:21:05 +0200

    Delete: exit.c

--- src/error.c
+++ src/error.c
@@ -6,11 +6,17 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 19:43:21 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/04 19:44:37 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/11 19:20:19 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+void	free_shell(t_shell *shell)
+{
+	if (shell->env != NULL)
+		free_matrix(shell->env);
+}
 
 void	exit_matrix(char *msg, char **matrix)
 {
