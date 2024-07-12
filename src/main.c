7bfd5fd02ac3843911eac845b58e00ea60c2dcf0
lgandarillas <lgandaf@gmail.com>
2024-07-12 19:48:49 +0200

    Add: Welcome message.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:43:33 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/12 18:45:47 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/12 19:47:48 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,19 @@ static void	shell_loop(t_shell *shell)
 	}
 }
 
+static void	print_welcome(void)
+{
+	printf("█▀▄▀█ █ █▄░█ █ █▀ █░█ █▀▀ █░░ █░░\n");
+	printf("█░▀░█ █ █░▀█ █ ▄█ █▀█ ██▄ █▄▄ █▄▄\n");
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	shell;
 
 	if (argc != 1 || !argv[0])
 		print_error("Error. Exec minishell with no arguments.\n");
+	print_welcome();
 	init_shell(&shell, envp);
 	shell_loop(&shell);
 	free_shell(&shell);
