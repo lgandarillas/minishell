ae000be975127fd57f6e99aad48f5d6ecca7c942
alejandquintero <alejandquinteroe@gmail.com>
2024-07-22 18:31:10 +0200

    update: executor: builtins structure

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,19 +6,21 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:31:40 by aquinter          #+#    #+#             */
-/*   Updated: 2024/07/22 17:53:20 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:16:08 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-typedef struct s_builtins
-{
-	/* data */
-}	t_builtins;
-
 typedef struct s_shell	t_shell;
+typedef int				(*t_builtin_func)(t_shell *shell);
+
+typedef struct s_builtin
+{
+	char			*name;
+	t_builtin_func	func;
+}	t_builtin;
 
 int					ft_cd(t_shell *shell);
 int					ft_pwd(t_shell *shell);
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,27 +6,44 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 20:40:33 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/22 17:56:22 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:26:58 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
+static void	init_builtins(t_builtin *builtins)
+{
+	builtins[0].name = "echo";
+	builtins[0].func = ft_echo;
+	builtins[1].name = "cd";
+	builtins[1].func = ft_cd;
+	builtins[2].name = "pwd";
+	builtins[2].func = ft_pwd;
+	builtins[3].name = "export";
+	builtins[3].func = ft_export;
+	builtins[4].name = "unset";
+	builtins[4].func = ft_unset;
+	builtins[5].name = "env";
+	builtins[5].func = ft_env;
+	builtins[6].name = "exit";
+	builtins[6].func = ft_exit;
+	builtins[7].name = NULL;
+	builtins[7].func = NULL;
+}
+
 int	execute_builtin(t_shell *shell)
 {
-	if (ft_strcmp(*(shell->cmd), "echo") == 0)
-		return (ft_echo(shell));
-	if (ft_strcmp(*(shell->cmd), "cd") == 0)
-		return (ft_cd(shell));
-	if (ft_strcmp(*(shell->cmd), "pwd") == 0)
-		return (ft_pwd(shell));
-	if (ft_strcmp(*(shell->cmd), "export") == 0)
-		return (ft_export(shell));
-	if (ft_strcmp(*(shell->cmd), "unset") == 0)
-		return (ft_unset((shell)));
-	if (ft_strcmp(*(shell->cmd), "env") == 0)
-		return (ft_env(shell));
-	if (ft_strcmp(*(shell->cmd), "exit") == 0)
-		return (ft_exit(shell));
+	int			i;
+	t_builtin	builtins[8];
+
+	i = 0;
+	init_builtins(builtins);
+	while (builtins[i].name != NULL)
+	{
+		if (ft_strcmp(shell->cmd[0], builtins[i].name) == 0)
+			return (builtins[i].func(shell));
+		i++;
+	}
 	return (FAILURE);
 }
