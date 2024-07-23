b5755cc036f3666ce948ffc0ec4c8c054bf0564c
lgandarillas <lgandaf@gmail.com>
2024-07-23 20:10:05 +0200

    Move: get_index()

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,26 +6,11 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 20:41:15 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/18 19:08:38 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/23 19:56:37 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
-
-int	get_index(char **env, const char *var)
-{
-	int	i;
-
-	i = 0;
-	while (env[i] != NULL)
-	{
-		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0)
-			return (i);
-		else
-			i++;
-	}
-	return (-1);
-}
 
 char	**parser(char *prompt)
 {
--- src/parser/path.c
+++ src/parser/path.c
@@ -6,11 +6,26 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 18:54:04 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/23 19:47:49 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/23 19:56:32 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+int	get_index(char **env, const char *var)
+{
+	int	i;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0)
+			return (i);
+		else
+			i++;
+	}
+	return (-1);
+}
 
 void	get_path(t_shell *shell)
 {
