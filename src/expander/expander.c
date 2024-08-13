c337d6500db48c9025adc49c9f2abe89196e1900
lgandarillas <lgandaf@gmail.com>
2024-08-13 14:52:30 +0200

    Init: expand_variables(char *node, char **env) in expander.c

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,17 +6,24 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:35:19 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/13 14:47:09 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/13 14:51:27 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void	expand_variables(char *node, char **env)
+{
+	(void)node;
+	(void)env;
+}
 
 void	expander(t_token *head, t_shell *shell)
 {
 	(void)shell;
 	while (head)
 	{
+		expand_variables(head->str, shell->env);
 		head = head->next;
 	}
 }
