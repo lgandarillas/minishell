e32cbc20d99cecba018c4a9fc345be8373a2b91e
Alejandro Quintero <alejandquinteroe@gmail.com>
2024-08-17 11:48:09 +0200

    update: parser print current token

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:24:23 by aquinter          #+#    #+#             */
-/*   Updated: 2024/08/13 16:27:17 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/08/15 09:14:24 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 typedef struct s_token	t_token;
 
-bool parser(t_token	*head);
+bool	parser(t_token	*head);
 
 #endif
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,11 +6,32 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 16:27:46 by aquinter          #+#    #+#             */
-/*   Updated: 2024/08/13 17:11:32 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/08/17 11:47:25 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void	syntax_error(char *str)
+{
+	ft_putstr_fd("syntax error near unexpected token ", STDERR_FILENO);
+	ft_putchar_fd('`', STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putchar_fd('\'', STDERR_FILENO);
+}
+
+static void	print_current(t_token *current)
+{
+	printf("===================================\n");
+	printf("%s\n", current->str);
+	printf("%d append\n", current->is_append);
+	printf("%d command\n", current->is_command);
+	printf("%d heredoc\n", current->is_heredoc);
+	printf("%d pipe\n", current->is_pipe);
+	printf("%d redirect in\n", current->is_redirect_in);
+	printf("%d redirect out\n", current->is_redirect_out);
+	printf("===================================\n");
+}
 
 bool	parser(t_token *head)
 {
@@ -19,22 +40,14 @@ bool	parser(t_token *head)
 	current = head;
 	while (current)
 	{
-		printf("===================================\n");
-		printf("%s\n", current->str);
-		printf("%d append\n", current->is_append);
-		printf("%d command\n", current->is_command);
-		printf("%d heredoc\n", current->is_heredoc);
-		printf("%d pipe\n", current->is_pipe);
-		printf("%d redirect in\n", current->is_redirect_in);
-		printf("%d redirect out\n", current->is_redirect_out);
-		printf("===================================\n");
-
-		if (!current->is_command && !current->next->is_command)
+		print_current(current);
+		if (!current->is_command)
 		{
-			ft_putstr_fd("syntax error near unexpected token ", STDERR_FILENO);
-			ft_putchar_fd('`', STDERR_FILENO);
-			ft_putstr_fd(current->str, STDERR_FILENO);
-			ft_putchar_fd('\'', STDERR_FILENO);
+			if (current->is_redirect_in)
+			{
+				if (ft_strlen(current->str) > 2)
+					syntax_error(current->str);
+			}		
 		}
 
 		current = current->next;
