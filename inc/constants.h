51ea2a57eb057c44145e8458dbaa3126744ed14b
Alejandro Quintero <alejandquinteroe@gmail.com>
2024-08-21 18:18:54 +0200

    update: new constant SYNTAX_ERROR

--- inc/constants.h
+++ inc/constants.h
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 19:17:03 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/06 14:17:22 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/08/19 11:53:16 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # define RESET "\x1b[0m"
 # define SUCCESS 0
 # define FAILURE 1
+# define SYNTAX_ERROR 2
 
 #endif
