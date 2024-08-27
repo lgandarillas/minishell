cf0523c89889c0ea3dddcd8efa11572cd72cc99a
alejandquintero <alejandquinteroe@gmail.com>
2024-08-27 16:50:17 +0200

    feat: testing redirect input

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 20:40:33 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/25 15:10:25 by aquinter         ###   ########.fr       */
+/*   Updated: 2024/08/27 16:45:52 by aquinter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ void	execute_cmd(t_shell *shell)
 	int		i;
 	char	*tmp;
 
+	shell->cmd++;
 	i = 0;
 	if (access(shell->cmd[0], X_OK) == SUCCESS)
 		execve(shell->cmd[0], shell->cmd, shell->env);
@@ -68,7 +69,10 @@ int	execute(t_shell *shell)
 {
 	int		status;
 	int		pid;
+	int		input;
 
+	input = open(shell->node->next->argv[0], O_RDONLY);
+	dup2(input, STDIN_FILENO);
 	if (!shell->cmd[0])
 		return (SUCCESS);
 	while (shell->node)
@@ -90,5 +94,6 @@ int	execute(t_shell *shell)
 		}
 		shell->node = shell->node->next;
 	}
+	close(input);
 	return (status);
 }
