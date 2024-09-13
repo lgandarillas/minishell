143a74711402ba31cb1f996461ab4079e7302088
lgandarillas <lgandaf@gmail.com>
2024-09-13 12:03:01 +0200

    Update: check_expand_heredoc()

--- src/heredoc/heredoc_expander.c
+++ src/heredoc/heredoc_expander.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:15:15 by lgandari          #+#    #+#             */
-/*   Updated: 2024/09/13 11:42:00 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/09/13 12:00:27 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,32 @@ static int	count_heredocs(t_lexer *node)
 void	check_expand_heredoc(t_lexer *node, t_shell *shell)
 {
 	int	num_heredocs;
+	int	i;
 
 	num_heredocs = count_heredocs(node);
 	shell->expand_heredoc = malloc(sizeof(bool) * (num_heredocs + 1));
 	if (!shell->expand_heredoc)
 		return ;
 	//FILL ARRAY
-	(void)shell;
+	i = 0;
+	while (node)
+	{
+		if (node->is_heredoc)
+		{
+			if (node->next && node->next->argv && node->next->argv[0])
+			{
+				if (node->next->argv[0][0] == '\'' || node->next->argv[0][0] == '\"')
+					shell->expand_heredoc[i] = false;
+				else
+					shell->expand_heredoc[i] = true;
+			}
+			else
+				shell->expand_heredoc[i] = true;
+			i++;
+		}
+		node = node->next;
+	}
+	shell->expand_heredoc[i] = false;
 }
 
 char	*expand_heredoc_line(char *line, t_shell *shell)
