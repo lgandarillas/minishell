d2389a96cd699b4da83d5e1c2261941cd7e79e1c
lgandarillas <lgandaf@gmail.com>
2024-11-03 18:44:05 +0100

    Refactor: expander.c

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 13:35:19 by lgandari          #+#    #+#             */
-/*   Updated: 2024/09/13 12:34:19 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/11/03 18:39:42 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,28 +104,36 @@ char	*expand_variables(char *str, char **env)
 	return (result);
 }
 
-void	expander(t_lexer *lexer_node, t_shell *shell)
+static int	update_lexer_node(t_lexer *lexer_node, t_shell *shell)
 {
 	char	*expanded_str;
 	char	*expanded_final;
+
+	expanded_str = expand_variables(lexer_node->str, shell->env);
+	if (!expanded_str)
+		return (FAILURE);
+	free(lexer_node->str);
+	lexer_node->str = expanded_str;
+	expanded_final = expand_status(lexer_node->str, shell->status);
+	if (expanded_final)
+	{
+		free(lexer_node->str);
+		lexer_node->str = expanded_final;
+	}
+	lexer_node->argv = word_splitter(lexer_node->str);
+	return (SUCCESS);
+}
+
+void	expander(t_lexer *lexer_node, t_shell *shell)
+{
 	char	**new_args;
 	t_lexer	*head;
 
 	head = lexer_node;
 	while (lexer_node)
 	{
-		expanded_str = expand_variables(lexer_node->str, shell->env);
-		if (!expanded_str)
+		if (update_lexer_node(lexer_node, shell) == FAILURE)
 			return ;
-		free(lexer_node->str);
-		lexer_node->str = expanded_str;
-		expanded_final = expand_status(lexer_node->str, shell->status);
-		if (expanded_final)
-		{
-			free(lexer_node->str);
-			lexer_node->str = expanded_final;
-		}
-		lexer_node->argv = word_splitter(lexer_node->str);
 		new_args = quote_cleaner(lexer_node->argv);
 		lexer_node->old_args = lexer_node->argv;
 		lexer_node->argv = new_args;
